2eb2d11c9943c442791059aa419ebcbb2f99e5f5
omerhussain <omerhussain.moh@gmail.com>
2023-01-04 20:26:39 +0500

    remove extra packages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBzvYlKLtlEgc28oGI9DX4wAjSSDWNzfS4",
+    authDomain: "angular-test-1e8ca.firebaseapp.com",
+    projectId: "angular-test-1e8ca",
+    storageBucket: "angular-test-1e8ca.appspot.com",
+    messagingSenderId: "104090774381",
+    appId: "1:104090774381:web:2f9ef98f5e5befad110864",
+    measurementId: "G-498P7PLXW9"
+  }
 };
